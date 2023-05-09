842bba46ea8e747596d6ee49e01dfd9d3f5922af
mesharikw85 <mkwg85@gmail.com>
2023-05-09 22:29:37 +0300

    task BMI-Calculator

--- index.js
+++ index.js
@@ -8,4 +8,51 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+height  = height / 100;
+let calculateBMI = weight / (height*height);
+
+
+
+
+alert (`your bmi is ${calculateBMI}`);
+
+
+ 
+ 
+
+if (calculateBMI < 18.5) {
+  calculateBMI = "Underweight";
+} else if (calculateBMI >= 18.5 && calculateBMI <= 24.9) {
+  calculateBMI = "Healthy Weight";
+} else if (calculateBMI >= 25 && calculateBMI <= 29.9) {
+  calculateBMI = "Overweight";
+} else {
+  calculateBMI = "Obesity";}
+  
+  alert (`your bmi is ${calculateBMI}`);
+
+  if (age >= 19 && age <= 65) {
+    if (calculateBMI >= 14 && calculateBMI <= 18.5) {
+      calculateBMI = "Healthy";
+    } else {
+      calculateBMI = "Not Healthy";
+    }
+  } else if (age >= 6 && age <= 19) {
+    if (calculateBMI >= 15 && calculateBMI <= 24) {
+      calculateBMI = "Healthy";
+    } else {
+      calculateBMI = "Not Healthy";
+    }
+  } else {
+    if (bmi >= 18.5 && bmi <= 24.9) {
+      calculateBMI = "Healthy";
+    } else {
+      calculateBMI = "Not Healthy";
+    }
+  }
+  
+  alert(`Your BMI is  ${calculateBMI}`);
 }
+
+
+ 
